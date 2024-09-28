5a49181d65cedf20007485dd96c203d5395355f3
Jeza <jeza@vip.qq.com>
2024-09-28 14:11:19 +0800

    Remove meaningless `@functools.wraps`

--- importhook/__init__.py
+++ importhook/__init__.py
@@ -68,7 +68,6 @@ def on_import(module_name, func=None):
         importhook.on_import('httplib', on_httplib_import)
     """
     if func is None:
-        @functools.wraps(func)
         def decorator(func):
             registry[module_name] = func
             return func
